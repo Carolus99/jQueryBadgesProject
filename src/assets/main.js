44cbae63fd940b6844a0e4f5a0bd169c9f923fec
Carolus99 <chasecomfort@gmail.com>
2017-01-21 13:33:52 -0800

    asdf

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,27 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+  url:'https://www.codeschool.com/users/Carolus99.json',
+  dataType:'jsonp',
+  success(response){
+    <div class="course">
+      <h3></h3>
+    <div class="course">
+
+    </div>
+
+        <h3></h3>
+    <div class="course">
+    </div>
+
+        <h3></h3>
+    </div>
+
+    <div class="course">
+        <h3></h3>
+    </div>
+  }
+
+  });
 
 });
